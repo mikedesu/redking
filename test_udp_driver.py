c2473752bc87e2fb09dc0a067e314e1a42694d49
darkmage <165251+mikedesu@users.noreply.github.com>
2025-02-24 17:41:27 -0600

    testing async UDP

--- test_udp_driver.py
+++ test_udp_driver.py
@@ -8,8 +8,8 @@ async def main():
         lambda: EchoUDPProtocol(), local_addr=("127.0.0.1", 8888)
     )
     try:
-        await asyncio.sleep(3600)  # Keep the server running for an hour
-        # await asyncio.Event().wait()
+        # await asyncio.sleep(3600)  # Keep the server running for an hour
+        await asyncio.Event().wait()
     finally:
         transport.close()
 
